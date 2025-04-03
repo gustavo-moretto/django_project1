b00749fee037124ae7f541a288a6ccd8ac5985cf
gustavo-moretto <gustavomorlin92@gmail.com>
2025-04-02 21:44:41 -0300

    restore

---
+++ recipes/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpResponse
+from recipes.views import home, contato, sobre
+
+
+urlpatterns = [    
+    path('', home),
+    path('contato/', contato),
+    path('sobre/', sobre),
+]
