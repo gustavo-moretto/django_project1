a6c65993e791161f8683a4426c9392810dcb0c00
Gustavo M Moretto <gustavomorlin92@gmail.com>
2025-04-14 12:43:19 -0300

    entendendo um pouco de html e css

--- recipes/views.py
+++ recipes/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Home')
+    # django takes the views from template folder
+    # you don't need specify the path
+    return render(request, 'recipes/home.html') 
 
 def contato(request):
-    return HttpResponse('Contato')
+    return render(request, 'index.html')
 
 def sobre(request):
     return HttpResponse('Sobre')
